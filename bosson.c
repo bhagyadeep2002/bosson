b7a1145a66423fc9af5d449ed4e7c21c4d9811a9
bhagyadeep1 <bhagyadeep@indigitalit.com>
2023-10-06 11:30:38 +0530

    added horizontal scrolliing

--- bosson.c
+++ bosson.c
@@ -38,6 +38,7 @@ typedef struct erow{
 struct editorConfig{
     int cx,cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -218,6 +219,12 @@ void editorScroll(){
     if (E.cy >= E.rowoff + E.screenrows){
         E.rowoff = E.cy - E.screenrows + 1;
     }
+    if (E.cx < E.coloff){
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols){
+        E.coloff = E.cx - E.screencols + 1;
+    }
 }
 
 void editorDrawRows(struct abuf *ab){
@@ -244,9 +251,10 @@ void editorDrawRows(struct abuf *ab){
         }
         }
         else {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[filerow].chars, len);
+            abAppend(ab, E.row[filerow].chars[E.coloff], len);
         }
         abAppend(ab, "\x1b[K",3);
         if (y < E.screenrows-1){
@@ -267,7 +275,7 @@ void editorRefreshScreen(){
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy-E.rowoff)+1, E.cx+1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy-E.rowoff)+1, (E.cx-E.coloff)+1);
     abAppend(&ab, buf, strlen(buf));
     abAppend(&ab,"\x1b[?25h",6);
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -277,6 +285,8 @@ void editorRefreshScreen(){
 /* input */
 
 void editorMoveCursor(int key){
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key){
         case ARROW_LEFT:
         if (E.cx != 0){
@@ -284,9 +294,9 @@ void editorMoveCursor(int key){
             }
             break;
         case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1){
+        if (row && E.cx < row->size){
             E.cx++;
-            }
+        }
             break;
         case ARROW_UP:
         if (E.cy != 0){
@@ -299,6 +309,13 @@ void editorMoveCursor(int key){
             }
             break;
     }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen){
+        E.cx = rowlen;
+    }
+
 }
 
 
@@ -340,6 +357,7 @@ void initEditor(){
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) die ("getWindowSize");
